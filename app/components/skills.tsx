0ad109b931a49254881f4b1fc4ef176e047cd8e8
unknown <amanuelfrc@gmail.com>
2026-03-21 12:42:17 +0300

    first-commit

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -9,48 +9,48 @@ export default function Skills() {
         <div className="md:col-span-5 col-span-12">
           <div className="md:sticky md:h-1/2 md:top-10">
             <div className="grid md:grid-cols-2 grid-cols-2 gap-6">
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 HTML
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 CSS
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Javascript
               </div>
 
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Typescript
               </div>
 
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 React
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Next.js
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Node.js
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Tailwind CSS
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Redux
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 REST-FULL API
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 Prisma ORM
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 SQL
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 MONGO DB
               </div>
-              <div className="rounded-xl border border-white text-white px-3 py-2.5 text-center">
+              <div className="rounded-xl  text-white px-3 py-2.5 text-center">
                 POSTGRESS SQL
               </div>
             </div>
